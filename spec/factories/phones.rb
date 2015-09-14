1970aecb202ae293be5fef8dfadace784e44c20e
baothi <baothi246@gmail.com>
2015-09-14 11:12:22 +0700

    Signed-off-by: baothi <baothi246@gmail.com>

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -2,7 +2,21 @@
 
 FactoryGirl.define do
   factory :phone do
-    phone "MyString"
-    phone_type "MyString"
+    association :contact
+    # phone { Faker::PhoneNumber.phone_number }
+    phone { '123-555-1234' }
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+    
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
+
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,50 +3,36 @@ require 'rails_helper'
 RSpec.describe Contact, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe Contact do
-    it "is valid with a firstname, lastname and email" do
-      contact = Contact.new(
-      	firstname: 'Aaron',
-      	lastname: 'Sumner',
-      	email: 'tester@example.com')
-      expect(contact).to be_valid
+
+    it "is has a valid factory" do
+      # expect(FactoryGirl.build(:contact)).to be_valid
+      expect(build(:contact)).to be_valid
     end
 
+    # it "is valid with a firstname, lastname and email" do
+    #   contact = Contact.new(
+    #     firstname: 'Aaron',
+    #     lastname: 'Sumner',
+    #     email: 'tester@example.com')
+    #   expect(contact).to be_valid
+    # end
+
     it "is invalid without a firstname" do
-      contact = FactoryGirl.build(:contact , firstname: nil)
+      contact = build(:contact , firstname: nil)
       contact.valid?
       expect(contact.errors[:firstname]).to include("can't be blank")
     end
 
     it "is invalid without a lastname" do
-      contact = FactoryGirl.build(:contact , lastname: nil)
+      contact = build(:contact , lastname: nil)
       contact.valid?
       expect(contact.errors[:lastname]).to include("can't be blank")
-    end
-
-    it "is invalid with a duplicate email address" do
-      Contact.create(
-        firstname: 'Joe', lastname: 'Tester',
-        email: 'tester@example.com'
-      )
-      contact = Contact.new(
-        firstname: 'Jane', lastname: 'Tester',
-        email: 'tester@example.com'
-      )
-      contact.valid?
-      expect(contact.errors[:email]).to include("has already been taken")
     end
 
     it "is invalid without an email address" do
       contact = FactoryGirl.build(:contact, email: nil)
       contact.valid?
       expect(contact.errors[:email]).to include("can't be black")
-    end
-
-    it "is invalid with a duplicate email address" do
-      FactoryGirl.create(:contact , email: 'aaron@example.com')
-      contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
-      contact.valid?
-      expect(contact.errors[:email]).to include('has already been taken')
     end
 
     # earlier validation examples omitted ..
@@ -56,12 +42,37 @@ RSpec.describe Contact, :type => :model do
         lastname: "Smith")
       expect(contact.name).to eq 'Jane Smith'
     end
-    
-    describe Contact do
-      it "has a valid factory" do
-        expect(FactoryGirl.build(:contact)).to be_valid
-      end
+
+    it "is invalid with a duplicate email address" do
+      # Contact.create(
+      #   firstname: 'Joe', lastname: 'Tester',
+      #   email: 'tester@example.com'
+      # )
+      # contact = Contact.new(
+      #   firstname: 'Jane', lastname: 'Tester',
+      #   email: 'tester@example.com'
+      # )
+      # contact.valid?
+      # expect(contact.errors[:email]).to include("has already been taken")
+      FactoryGirl.create(:contact, email: 'aaron@example.com')
+      contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
+      contact.valid?
+      expect(contact.errors[:email]).to include('has already been taken')
+
     end
+
+    it "is invalid with a duplicate email address" do
+      FactoryGirl.create(:contact , email: 'aaron@example.com')
+      contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
+      contact.valid?
+      expect(contact.errors[:email]).to include('has already been taken')
+    end
+
+    
+    # it "has three phone numbers" do
+    #   expect(create(:contact).phones.count).to eq 3
+    # end
+
    
 
     describe "filter last name by letter" do
